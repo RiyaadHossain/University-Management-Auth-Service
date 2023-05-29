0afef1819ad89e571723d60f251b07709745f8be
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-05-29 09:28:16 +0600

    Project set up completed

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import config from "./config";
+import app from "./app";
+
+mongoose
+  .connect(config.MONGODB_URL as string)
+  .then(() => console.log("Database connceted successfully 💯"))
+  .catch((err) => console.log(err));
+
+app.listen(config.PORT, () => {
+  console.log(`Application is listening ✅`);
+});
