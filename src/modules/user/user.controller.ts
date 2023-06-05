fb787140b1a1482c30d4455043ccbe28ac5ce099
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-06-05 09:03:31 +0600

    Zod validation

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,18 @@
+import { z } from 'zod'
 import { RequestHandler } from 'express'
 import { UserService } from './user.services'
 
 const createUser: RequestHandler = async (req, res, next) => {
   try {
+    // Zod Validation
+    const userZodValidation = z.object({
+      body: z.object({
+        role: z.string({ required_error: 'Role is required' }),
+      }),
+    })
+
+    userZodValidation.parse(req)
+
     const { user } = req.body
     const result = await UserService.createUser(user)
 
