3ed3ec7b4ec2474850b4181cd39806c58583fb5d
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-06-07 07:52:46 +0600

    change in academic semester controller for previous refactoring

--- src/modules/academicSemester/academicSemester.controller.ts
+++ src/modules/academicSemester/academicSemester.controller.ts
@@ -1,23 +1,27 @@
 import { RequestHandler } from 'express'
 import { AcademicSemesterService } from './academicSemester.services'
+import catchAsync from '../../catchAsync'
+import sendResponse from '../../shared/sendResponse'
+import httpStatus from 'http-status-codes'
 
-const createacAdemicSemester: RequestHandler = async (req, res, next) => {
-  try {
+const createacAdemicSemester: RequestHandler = catchAsync(
+  async (req, res, next) => {
     const academicSemester = req.body
 
     const result = await AcademicSemesterService.createAcademicSemester(
       academicSemester
     )
 
-    res.status(200).json({
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
       success: true,
       message: 'Academic Semester created successfully!',
       data: result,
     })
-  } catch (error) {
-    next(error)
+
+    next()
   }
-}
+)
 
 export const AcademicSemesterController = {
   createacAdemicSemester,
