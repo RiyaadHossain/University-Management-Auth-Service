c77ac7fa763f2624c45ef4beb6b4d2b1fe75f794
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-06-12 09:16:22 +0600

    Update User Model

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,9 +1,12 @@
-import { Model } from 'mongoose'
+import { Model, ObjectId } from 'mongoose'
 
 export type IUser = {
   id: string
   role: string
   password: string
+  student?: ObjectId
+  faculty?: ObjectId
+  admin?: ObjectId
 }
 
 export type UserModel = Model<IUser, Record<string, unknown>>
